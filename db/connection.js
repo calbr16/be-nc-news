154de8bdd7d5c3c888f793dad1d2f2bff2c18de8
calbr16 <callisto1619@gmail.com>
2024-07-19 15:24:00 +0100

    update connection.js and package.json

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,6 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-const config = {};
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
