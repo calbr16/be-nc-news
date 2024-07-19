2069361ddc7b9fc9498c780c1476df9df3bc5ae3
calbr16 <callisto1619@gmail.com>
2024-07-19 14:40:13 +0100

    updated connection.js, listen.js and package.json to allow for API hosting

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require("./app")
 
-app.listen(9000, () => {
-    console.log(`Server is listening on port 9000`);
-});
+const { PORT = 9000 } = process.env;
+
+app.listen(PORT, () => console.log('Listening on ${PORT}...'));
 
